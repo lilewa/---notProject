9e53fdfde8d969bb2b92e091318818c36719c9a2
lilewa <524612604@qq.com>
2017-11-18 11:42:08 +0800

    go
    go

---
+++ 随意/b.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang=" en">
+ <head>  
+ <meta charset=" UTF- 8"/> 
+ <title> 第 1 章 1. 1 节</title> 
+ <script src=" js/ jquery- 1. 11. 2. min. js"></script> 
+ <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script> 
+ <script src=" js/cookie. js"></script> 
+ <script src=" js/wxJSSDK. js"></script> 
+ </head> 
+   <body> 
+ <h1 style=" font- size: 40px">:)</h1> 
+ <b style=" font- size: 20px"> JSSDK 部署 学习！</b> 
+ </body>  
+</html>
